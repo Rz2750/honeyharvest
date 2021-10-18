3452bf6e25b4700836000729767b2f9fa8837be1
sikerv <60186957+sikerv@users.noreply.github.com>
2021-10-18 16:35:16 -0400

    collision
    attempts

--- Honey Harvest/Assets/CheckCollide.cs
+++ Honey Harvest/Assets/CheckCollide.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class CheckCollide : MonoBehaviour
 {
+    public GameObject BeePurple;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BeePurple.SetActive(false);
     }
 
     // Update is called once per frame
     void Update () {
-    
+      
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BeePurple.SetActive(true);
+    }
+
 }
+
